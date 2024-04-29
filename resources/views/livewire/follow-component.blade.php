947fcf7c7755f53fa4df417e58419bc020c71906
shadrack mballah <shadrackmballah80@gmail.com>
2024-04-29 13:26:40 +0300

    test

--- resources/views/livewire/follow-component.blade.php
+++ resources/views/livewire/follow-component.blade.php
@@ -1,1 +1,1 @@
-<button class="btn btn-outline-primary btn-sm m-2" wire:click.prevent="followUnfollow({{$followed_id}})"> {{$IFollowed == true ? 'unfollow' : 'follow'}} {{$number_followers}}</button>
+<button class="btn btn-outline-primary btn-sm m-2" wire:click.prevent="followUnfollow({{$followed_id}})"> {{$IFollowed == true ? 'defollow' : 'follow'}} {{$number_followers}}</button>
