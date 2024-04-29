fef6e2c8198cbee25d2bce9b6230f87446301658
shadrack mballah <shadrackmballah80@gmail.com>
2024-04-29 13:14:37 +0300

    updated the follow and unfollow text

--- app/Livewire/UserProfile.php
+++ app/Livewire/UserProfile.php
@@ -14,6 +14,16 @@ class UserProfile extends Component
         ->where('user_profiles.user_id',auth()->user()->id)
         ->first();
     }
+
+     public function placeholder(){
+        return <<<'HTML'
+            <button class="btn btn-primary" type="button" disabled>
+                <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+                <span class="visually-hidden">Loading...</span>
+              </button>
+        HTML;
+    }
+
     public function render()
     {
         return view('livewire.user-profile');
--- resources/views/livewire/follow-component.blade.php
+++ resources/views/livewire/follow-component.blade.php
@@ -1,1 +1,1 @@
-<button class="btn btn-outline-primary btn-sm m-2" wire:click.prevent="followUnfollow({{$followed_id}})"> {{$IFollowed == true ? 'followed' : 'follow'}} {{$number_followers}}</button>
+<button class="btn btn-outline-primary btn-sm m-2" wire:click.prevent="followUnfollow({{$followed_id}})"> {{$IFollowed == true ? 'unfollow' : 'follow'}} {{$number_followers}}</button>
